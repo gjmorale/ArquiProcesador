f862fd301db8783ea1f099240422af671afa7439
Nebil Kawas <nkawas90@gmail.com>
2015-10-01 23:43:59 -0300

    Added BBC & The Guardian readers.

--- RSS/RSSFeed.py
+++ RSS/RSSFeed.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 import feedparser
+import requests
 import urllib
 
 #This class contains all the readers of RSS
@@ -39,3 +40,103 @@ def la_tercera_reader():
                     'content': content_string,
                     'category': category}
     return news
+
+import requests
+
+# fix: juntar todo el código duplicado.
+#      pido perdón por este *code smell*.
+# *topic* puede ser '', '/world', '/politics', entre otros.
+def bbc_reader(id_start, topic):
+    feed_url = "http://feeds.bbci.co.uk/news" + topic + "/rss.xml"
+    fdict = feedparser.parse(feed_url)
+
+    # nota: *fdict* es un diccionario.
+    # ===== ==========================
+    # print(fdict.keys()) aquí están los elementos disponibles.
+    # sin embargo, los más importantes son: *feed* y *entries*.
+    # - *feed* es un diccionario de metadatos.
+    # - *entries* es una lista de noticias.
+
+    new_id = id_start
+    news = {} # diccionario donde se almacenarán las noticias.
+    feed = fdict.feed
+    print("Título:", feed.title)
+    print("Descripción:", feed.description)
+
+    entries = fdict.entries
+    for entry in entries:
+        print(entry.id)            # enlace (que será usado por *requests*)
+        print(entry.title)         # título
+        print(entry.summary)       # resumen
+        print(entry.published)     # fecha de publicación
+        # print(entry.description) # lo mismo que *summary*
+
+        link = entry.id
+        title = entry.title
+        pub_date = entry.published
+        try:
+            r = requests.get(entry.id)
+            soup = BeautifulSoup(r.content, 'html.parser')
+            story_body = soup.find('div', class_='story-body')
+            article_text = story_body(['p', 'h1', 'h2'])
+            news[new_id] = {'link': link,
+                            'title': title,
+                            'pubDate': pub_date,
+                            'content': article_text}
+        except:
+            print('Error Inesperado')
+
+        new_id += 1
+    # print(news)
+    return news
+
+# bbc_reader(0, '')
+
+# the guardian.
+# *topic* puede ser '', '/football', '/football/liverpool', entre otros.
+def tgd_reader(id_start, topic):
+    feed_url = "http://www.theguardian.com" + topic + "/rss"
+    fdict = feedparser.parse(feed_url)
+
+    # nota: *fdict* es un diccionario.
+    # ===== ==========================
+    # print(fdict.keys()) aquí están los elementos disponibles.
+    # sin embargo, los más importantes son: *feed* y *entries*.
+    # - *feed* es un diccionario de metadatos.
+    # - *entries* es una lista de noticias.
+
+    new_id = id_start
+    news = {} # diccionario donde se almacenarán las noticias.
+    feed = fdict.feed
+    print("Título:", feed.title)
+    print("Descripción:", feed.description)
+
+    entries = fdict.entries
+    for entry in entries:
+        print(entry.id)            # enlace (que será usado por *requests*)
+        print(entry.title)         # título
+        print(entry.summary)       # resumen
+        print(entry.published)     # fecha de publicación
+        # print(entry.description) # lo mismo que *summary*
+
+        link = entry.id
+        title = entry.title
+        pub_date = entry.published
+        try:
+            r = requests.get(entry.id)
+            soup = BeautifulSoup(r.content, 'html.parser')
+            story_body = soup.find('div', class_='content__article-body')
+            article_text = story_body(['p', 'h1', 'h2'])
+            news[new_id] = {'link': link,
+                            'title': title,
+                            'pubDate': pub_date,
+                            'content': article_text}
+            print(news)
+        except:
+            print('Error Inesperado')
+
+        new_id += 1
+    # print(news)
+    return news
+
+# tgd_reader(0, '/football/liverpool')
