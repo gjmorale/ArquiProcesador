8065820e5b6948192404943045301b0b641d14e6
Diego A. Fern ndez <difernandez@uc.cl>
2015-10-19 23:19:20 -0400

    Keyword extraction from title (almost) done

--- NewsProcessor/Processor.py
+++ NewsProcessor/Processor.py
@@ -1,8 +1,11 @@
-import re
+import nltk
+from nltk.corpus import wordnet as wn
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import stopwords as sw
+from nltk import word_tokenize as wt
 
 people = []
 places = []
-events = []
 
 #TODO: extend people and places list
 people.extend(["Michelle Bachelet",
@@ -20,15 +23,7 @@ places.extend(["Chile",
 				"Estados Unidos",
 				"E.E.U.U"])
 
-events.extend(["terremoto",
-				"concierto",
-				"guerra",
-				"festival",
-				"batalla",
-				"homicidio",
-				"elección"])
-
-def find_similar (news, person):
+def find_similar (self, news, person):
 	#Allows names to be written with a middle name or with first name abbreviated (like P. Sherman)
 	palabras = person.split()
 	if len(palabras) == 2:
@@ -43,28 +38,56 @@ def find_similar (news, person):
 	else:
 		return news.find(person)
 
-def people_filter (news, tags):
+def people_filter (self, news, tags):
 	for person in people:
 		if find_similar(news, person) != -1:
 			tags[person] = "Personaje"
 
-def places_filter (news, tags):
+def places_filter (self, news, tags):
 	for place in places:
 		if news.find(place) != -1:
 			tags[place] = "Lugar"
 
-def events_filter (news, tags):
-	#TODO: more sophisticated approach (keywords with RAKE algorithm? clustering?)
-	for event in events:
-		if news.find(event) != -1:
-			tags[event] = "Suceso"
+def events_filter (self, title, lang, tags):
+	#Regex adapted from nltk documentation
+	pattern = (r"(?x)"      # set flag to allow verbose regexps
+	    r"(?:[A-Z])(?:\.[A-Z])+\.?"  # abbreviations, e.g. U.S.A.
+	    r"|\w+(?:-\w+)*"            # words with optional internal hyphens
+	    r"|\$?\d+(?:\.\d+)?%?"      # currency and percentages, e.g. $12.40, 82%
+	    )
 
-def filter (news):
+	#Tokenize title acording to the regex pattern.
+	tokens = nltk.regexp_tokenize(title, pattern)
+
+	#Remove stopwords. Lang should be either 'english' or 'spanish'.
+	tokens = [w.lower() for w in tokens if w.lower() not in sw.words()] 
+
+	#Lemmatization for english. For Spanish, just stemming (?) (TODO!!).
+	wnl = WordNetLemmatizer()
+
+	#Tag words (noun, adjective, verb or adverb). Makes lemmatization more accurate.
+	pos_toks = nltk.pos_tag(tokens)
+	wordnet_tag ={'NN':'n', 'NNS':'n', 'NNP':'n', 'NNPS':'n',
+				'JJ':'a', 'JJR':'a', 'JJS':'a',
+				'VB':'v', 'VBD':'v', 'VBG':'v', 'VBN':'v', 'VBP':'v', 'VBZ':'v',
+				'RB':'r', 'RBR':'r', 'RBS':'r'}
+
+	#Lemmatization, with pos tags.
+	for i in range(len(pos_toks)):
+		pos_tok = pos_toks[i]
+		print (i, pos_tok)
+		if pos_tok[1] in wordnet_tag.keys():
+			tokens[i] = wnl.lemmatize(tokens[i], wordnet_tag[pos_tok[1]])
+		else:
+			tokens[i] = wnl.lemmatize(tokens[i])
+
+def filter (self, news, lang):
 	#Applies all filters
 	tags = {}
 	people_filter(news, tags)
 	places_filter(news, tags)
-	events_filter(news, tags)
+	#TODO: pass title to events_filter
+	#events_filter(title, lang, tags)
 	return tags
 
 #test = "M. Bachelet y Barack James Obama ademas tambien homicidio de alguien en Chile o quizas China"
